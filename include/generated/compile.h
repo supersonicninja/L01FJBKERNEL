e405904c3788125e8dbc231dea09a8c0ede2108d
Paul Reioux <reioux@gmail.com>
2013-11-25 20:59:26 -0600

    Add faux123 intellidemand 5.0

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Jan 4 06:31:32 PST 2014"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Jan 5 04:46:01 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv001-g8ad65af-dirty"
+#define UTS_RELEASE "3.4.0ninjakernelv016-gb58c79d-dirty"
