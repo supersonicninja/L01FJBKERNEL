8877a8f29c16b2806c35efaf67bf5146163b70f1
Pranav Vashi <neobuddy89@gmail.com>
2013-12-10 20:13:00 -0800

    smartassH3:add

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#15 SMP PREEMPT Tue Jan 7 21:06:36 PST 2014"
+#define UTS_VERSION "#16 SMP PREEMPT Wed Jan 8 17:27:11 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv080+"
+#define UTS_RELEASE "3.4.0ninjakernelv082+"
