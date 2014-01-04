24afdd46e9a9602381d78bfd179552c3e5c14507
supersonicninja <niconicotor@live.com>
2014-01-03 22:45:29 -0800

    L01F11k source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Sep 24 19:38:08 KST 2013"
+#define LINUX_COMPILE_BY "jaeho83.lim"
+#define LINUX_COMPILE_HOST "LGEARND1B1"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-perf-g5086eb7-dirty"
