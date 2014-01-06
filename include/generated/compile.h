7147bbd71def1387404597bb3401a51af756c98e
supersonicninja <niconicotor@live.com>
2014-01-06 02:21:52 -0800

    made voltage configurable

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Sun Jan 5 23:15:36 PST 2014"
+#define UTS_VERSION "#6 SMP PREEMPT Mon Jan 6 02:11:37 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv020-g2b86629-dirty"
+#define UTS_RELEASE "3.4.0ninjakernelv030-gfc0c435-dirty"
