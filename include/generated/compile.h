9aebb4fb2e432d00609327d1cf036a5f9c3b6cd8
supersonicninja <niconicotor@live.com>
2014-01-06 01:24:37 -0800

    intellidemand:enable in config

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Jan 5 04:46:01 PST 2014"
+#define UTS_VERSION "#5 SMP PREEMPT Sun Jan 5 23:15:36 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv016-gb58c79d-dirty"
+#define UTS_RELEASE "3.4.0ninjakernelv020-g2b86629-dirty"
