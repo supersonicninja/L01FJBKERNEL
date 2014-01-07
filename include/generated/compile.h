e8f0b213c0d7d21bdd479b64499d51c4b0318bd9
supersonicninja <niconicotor@live.com>
2014-01-06 17:53:17 -0800

    row:fix code

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Mon Jan 6 16:11:35 PST 2014"
+#define UTS_VERSION "#9 SMP PREEMPT Mon Jan 6 17:36:36 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv059-gdfe4938-dirty"
+#define UTS_RELEASE "3.4.0ninjakernelv059-g376bb0a-dirty"
