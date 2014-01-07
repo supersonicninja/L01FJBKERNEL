d00b592f75389e9b252ef5db4dbb90b17e7220e0
supersonicninja <niconicotor@live.com>
2014-01-07 05:25:21 -0800

    BFQ:made it working

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Mon Jan 6 21:29:14 PST 2014"
+#define UTS_VERSION "#13 SMP PREEMPT Tue Jan 7 04:55:58 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
