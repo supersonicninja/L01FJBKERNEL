e7171d75754b271515910f175ede87b096ea1504
supersonicninja <niconicotor@live.com>
2014-01-08 18:44:58 -0800

    cpufreq:made ondemand default governor

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 17 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#16 SMP PREEMPT Wed Jan 8 17:27:11 PST 2014"
+#define UTS_VERSION "#17 SMP PREEMPT Wed Jan 8 18:08:12 PST 2014"
 #define LINUX_COMPILE_BY "test"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0ninjakernelv082+"
+#define UTS_RELEASE "3.4.0ninjakernelv087+"
